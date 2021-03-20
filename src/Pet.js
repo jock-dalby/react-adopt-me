d26e267f990000d6649f57dead5b8be4c2088b56
jock-citrus <jock.dalby@citrusad.com>
2021-03-20 20:03:03 +1000

    convert Pet.js to JSX

--- src/Pet.js
+++ src/Pet.js
@@ -1,9 +1,24 @@
 import React from "react";
 
 export default function Pet({ name, animal, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
+  // To create markup this way requires us to visualise the HTML
+  // we want, and then write it in Javascript. JSX allows us to
+  // write the markup directly into the JS file (as below).
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, name),
+  //   React.createElement("h2", {}, animal),
+  //   React.createElement("h2", {}, breed),
+  // ]);
+  return (
+    // When below jsx runs through Babel, it gets transpiled into
+    // React.createElement calls (as above), it is just easier and
+    // more readable to write markup in HTML-like syntax. This means
+    // that wherever you have JSX code, you have to have React in
+    // scope.
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 }
