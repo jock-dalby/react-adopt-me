1804dd9dd996434eda1109a7d3ce8d3bc3187a73
jock-citrus <jock.dalby@citrusad.com>
2021-03-21 12:39:19 +1000

    update SearchParams location value using setState hook

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  // useState is a hook. All hooks handle stateful logic and
+  // are prefixed with 'use'.
+  const [location, setLocation] = useState("Seattle, WA");
 
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
         </label>
         <button>Submit</button>
       </form>
