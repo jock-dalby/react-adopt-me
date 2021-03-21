d6cce5e4a25fc6a36a885e8919aab2ace9c569fd
jock-citrus <jock.dalby@citrusad.com>
2021-03-22 08:59:22 +1000

    import ANIMALS from @frontendmasters and create animals select element

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+// @frontendmasters was not in package.json but Parcel is smart
+// enough to recognise that and go and grab from npm registry.
+// When this is saved, Parcel will install and add to
+// dependencies list.
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   // useState is a hook. All hooks handle stateful logic and
@@ -8,6 +13,7 @@ const SearchParams = () => {
   // be inconsistent with conditionals/unpredictable logic. "react-hooks" has
   // been added to eslint config to error against this behaviour.
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("Dog");
 
   return (
     <div className="search-params">
@@ -20,6 +26,20 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal) => (
+              <option value={animal}>{animal}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
