bbd0b50283cfc810d83e896f17b406297eb88591
jock-citrus <jock.dalby@citrusad.com>
2021-03-22 19:27:49 +1000

    add React.StrictMode and comments

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div id="app-test-id">
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    // StrictMode can be shipped to production. Does not render anything or
+    // increase bundle size. It will just give warnings if use any unstable
+    // or deprecated React apis. Helpful if working in legacy applications
+    // that do not use latest features.
+    <React.StrictMode>
+      <div id="app-test-id">
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   );
 };
 
