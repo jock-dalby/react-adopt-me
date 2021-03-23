8a3e9fd0ff5e17b80bc032694d28dc4f129dd466
jock-citrus <jock.dalby@citrusad.com>
2021-03-24 09:21:21 +1000

    JSON.stringify props and display in browser window

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-const Details = () => {
-  return <p>hi lol</p>;
+const Details = (props) => {
+  return (
+    // Great way to see all props that are coming in from router in the browser
+    <pre>
+      <code>{JSON.stringify(props, null, 4)}</code>
+    </pre>
+  );
 };
 
 export default Details;
